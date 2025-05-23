a759110c664a2d48193336c1670d9a147d6b3d8a
PrathamModi001 <Pratham.mce21@sot.pdpu.ac.in>
2025-05-23 17:25:36 +0530

    finals changes for deployment

--- next.config.js
+++ next.config.js
@@ -21,11 +21,6 @@ const nextConfig = {
     }
     return config;
   },
-  // Disable development indicators in production
-  devIndicators: {
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
-  },
   // Additional production optimizations
   poweredByHeader: false,
   reactStrictMode: true,
